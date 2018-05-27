7245b9feb9a14fffd3b6c00c046ef20cb647f826
Christophe Banga <christophebanga@gmail.com>
2018-05-27 17:39:10 +0300

    Add Class with function properties for play and hold

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,31 @@ $("button#start").click(function(){
 $("button#new-game").click(function(){
   $(".landing-page").hide();
   $(".game").show();
-
-
 })
+
+//Business logic
+class Player(){
+  this.roll = 0;
+  this.roundScore = 0;
+  this.totalScore= 0;
+  this.turn = 0;
+  this.playerName= 0;
+  //there are 2 possible outcomes roll= 1 and this round is over
+  //or roll !=1 and the player keeps playing
+  this.play= function(){
+    if(this.roll ===1){
+      this.roundScore = 0;
+      alert("1!"+this.playerName+ ", your turn is over!")
+      else{
+        this.roundscore += this.roll;
+      }
+    }
+  }
+  //what happens if player decides to skip his turn
+  this.hold= function(){
+    this.totalScore += this.roundScore;
+    this.roundScore = 0;
+    alert("Pass!")
+  }
+
+}
