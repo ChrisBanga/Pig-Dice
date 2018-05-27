cfc9aee8555abdb435e04d5297b9fb05df9ccd3e
Christophe Banga <christophebanga@gmail.com>
2018-05-27 17:21:11 +0300

    Add responsiveness to Start and New-game buttons

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$("button#start").click(function(){
+  $(".landing-page").show();
+  $(".game").hide();
+})
+$("button#new-game").click(function(){
+  $(".landing-page").hide();
+  $(".game").show();
+
+
+})
